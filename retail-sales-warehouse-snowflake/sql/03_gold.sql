2dec626d24cca071c37f53eb9a3e2fb07f24bc1a
Ganesh <ganeshnayak2508@gmail.com>
2026-02-27 08:33:58 +0000

    updated

---
+++ retail-sales-warehouse-snowflake/sql/03_gold.sql
@@ -0,0 +1,51 @@
+USE WAREHOUSE COMPUTE_WH;
+USE DATABASE SNOWFLAKE_LEARNING_DB;
+USE ROLE ACCOUNTADMIN;
+
+CREATE SCHEMA IF NOT EXISTS GOLD;
+
+-- Sales by State
+CREATE OR REPLACE TABLE GOLD.SALES_BY_STATE AS
+SELECT
+    d.state,
+    SUM(f.sales_price) AS total_sales
+FROM SILVER.FACT_SALES f
+JOIN SILVER.CUSTOMER_DIM d
+    ON f.customer_sk = d.customer_sk
+GROUP BY d.state;
+
+-- Sales by Year
+CREATE OR REPLACE TABLE GOLD.SALES_BY_YEAR AS
+SELECT
+    YEAR(transaction_date) AS sales_year,
+    SUM(sales_price) AS total_sales
+FROM SILVER.FACT_SALES
+GROUP BY sales_year;
+
+-- Monthly Sales
+CREATE OR REPLACE TABLE GOLD.MONTHLY_SALES AS
+SELECT
+    DATE_TRUNC('month', transaction_date) AS sales_month,
+    SUM(sales_price) AS total_sales
+FROM SILVER.FACT_SALES
+GROUP BY sales_month;
+
+-- Top 10 Stores
+CREATE OR REPLACE TABLE GOLD.TOP_STORES AS
+SELECT
+    store_id,
+    SUM(sales_price) AS total_sales
+FROM SILVER.FACT_SALES
+GROUP BY store_id
+ORDER BY total_sales DESC
+LIMIT 10;
+
+-- Validation Checks
+SELECT COUNT(*) FROM GOLD.MONTHLY_SALES;
+SELECT COUNT(*) FROM GOLD.TOP_STORES;
+SELECT COUNT(*) FROM GOLD.SALES_BY_STATE;
+SELECT COUNT(*) FROM GOLD.SALES_BY_YEAR;
+
+-- Revenue Reconciliation Check
+SELECT SUM(sales_price) FROM SILVER.FACT_SALES;
+SELECT SUM(total_sales) FROM GOLD.SALES_BY_STATE;
